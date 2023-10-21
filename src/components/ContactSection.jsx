d55d755b9c23df474a2494d2ee698910ddfa7341
wajurizvi <wajurizvi@gmail.com>
2023-10-21 18:37:48 +0500

    new changes email js

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,5 +1,5 @@
-import { useRef, useEffect, useState } from "react";
-import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import * as emailjs from "@emailjs/browser";
 
 
 
@@ -9,47 +9,32 @@ const ContactSection = () => {
 
 
 const [name,setName]=useState("")
-const [email,setemail]=useState("")
+const [email,setEmail]=useState("")
 const [message,setMessage]=useState("")
-const [loading,setLoading]=useState("")
 
-
-
-
-
-
-
-
-
-  useEffect(() => emailjs.init("WPYE5KqvgkGuzMNa_"), []);
-
-
- 
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const serviceId = "service_ir1tvmp";
-    const templateId = "template_o0utrcm";
-
-const emailParm={
-  from_name:name,
-  to_email:email,
-  message:message,
-
-}
-
-    try {
-      setLoading(true);
-      await emailjs.send(serviceId, templateId, emailParm {
+function Sendmail() {
   
-      });
-      alert("email successfully sent check inbox");
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+  const data={
+    to_name:name,
+    to_email:email,
+    message:message,
+  
+  } 
+ 
+    const Service_id = "service_ir1tvmp";
+    const Template_d = "template_o0utrcm";
+    const User_id   = "WPYE5KqvgkGuzMNa_";
+    emailjs.send(Service_id,Template_d,data,User_id).
+c    then(
+      function(response){
+        alert("send sucsess fully")
+      },
+      function(error){
+        console.log("errorc")
+      }
+    )
     }
-  }; 
+     
     return (
      
     <div id="contact" className="section">
@@ -65,10 +50,14 @@ const emailParm={
                     </div>
                     <div className="col-lg-6 col-md-6 col-xs-10 wow fadeInUp" data-wow-delay="0.6s">
   
-                        <form role="form" onSubmit={handleSubmit} method="#" action="#">
-                            <input  name="name" type="text" className="form-control" id="name" placeholder="Your Name" />
-                            <input name="email" type="email" className="form-control" id="email" placeholder="Your Email" />
-                            <textarea name="message" rows="5" className="form-control" id="message" placeholder="Your Message"></textarea>
+                        <form role="form" onSubmit={(e)=>
+                        {Sendmail()}} method="#" action="#">
+                            <input onChange={(e)=>
+                              {setName(e.target.value)}} name="name" type="text" className="form-control" id="name" placeholder="Your Name" />
+                            <input onChange={(e)=>
+                              {setEmail(e.target.value)}} name="email" type="email" className="form-control" id="email" placeholder="Your Email" />
+                            <textarea onChange={(e)=>
+                              {setMessage(e.target.value)}} name="message" rows="5" className="form-control" id="message" placeholder="Your Message"></textarea>
                             <button name="send" type="submit" className="form-control" id="send"  >submit</button>
                         </form>
                     </div>
